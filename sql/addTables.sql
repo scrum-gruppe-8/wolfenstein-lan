e51403985e7389fb3ac9658256df867353aa206d
Jørgen Andreas Bratland Bull <jorgenabb@gmail.com>
2024-04-22 11:33:50 +0200

    Added email to paamelding table

--- sql/addTables.sql
+++ sql/addTables.sql
@@ -3,6 +3,7 @@ create table paamelding
     id        int auto_increment,
     fornavn   tinytext not null,
     etternavn tinytext not null,
+    email     tinytext not null,
     constraint paamelding_pk
         primary key (id)
 );
