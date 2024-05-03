29a98b766fbc18824221bae4c7d59f227faef341
Jørgen Andreas Bratland Bull <jorgenabb@gmail.com>
2024-05-03 09:01:42 +0200

    Added header to table

--- paameldt.php
+++ paameldt.php
@@ -5,6 +5,7 @@ include_once "funksjoner/automagic.php";
 $page["title"] = "Påmeldte";
 
 $page["body"] = function () {
+    echo "<h1>Tabell for Påmeldte</h1>";
     echo automagic::automagicTable(getFromDatabase::table("paamelding"));
 };
 
